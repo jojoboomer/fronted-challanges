18d0c7e49fef58d48d4b6ecfee644664ca7330f8
jjlabrada <jjlabranate@gmail.com>
2025-01-05 19:09:56 -0500

    init

--- panda.config.ts
+++ panda.config.ts
@@ -1,20 +1,48 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
-
-  // Where to look for your css declarations
-  include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
-
-  // Files to exclude
+  // define the content to scan 👇🏻
+  include: [
+    "./src/**/*.{ts,tsx,js,jsx,astro}",
+    "./pages/**/*.{ts,tsx,js,jsx,astro}",
+    "src",
+  ],
   exclude: [],
-
-  // Useful for theme customization
-  theme: {
-    extend: {},
-  },
-
-  // The output directory for your css system
   outdir: "styled-system",
+  theme: {
+    extend: {
+      tokens: {
+        fonts: {
+          Outfit: { value: 'Outfit, sans-serif' }
+        },
+        colors: {
+          primary: {
+            DEFAULT: { value: "#0070f3" },
+            'nft-cyan': { value: "hsl(178, 100%, 50%)" },
+            'nft-blue': { value: "hsl(215, 51%, 70%)" },
+          },
+          secondary: {
+            DEFAULT: { value: "#ff4081" },
+          },
+          neutral: {
+            DEFAULT: { value: "#333" },
+            "nft-300": { value: "hsl(215, 32%, 27%)" },
+            "nft-500": { value: "hsl(216, 50%, 16%)" },
+            "nft-800": { value: "hsl(217, 54%, 11%)" },
+          },
+        },
+      },
+    },
+  },
+  globalCss: {
+    "html, body": {
+      position: "relative",
+      fontFamily: "body",
+      margin: 0,
+      padding: 0,
+      backgroundColor: "cian.400",
+      'box-sizing': 'border-box'
+    },
+  },
 });
---
+++ src/styles/nft.ts
@@ -0,0 +1,94 @@
+import { css } from "@styled-system/css";
+import { flex, square } from "@styled-system/patterns";
+
+export const main = flex({
+  position: "relative",
+  fontSize:'18px',
+  bg: "neutral.nft-800",
+  minHeight: "100vh",
+  flexDir: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontFamily: "Outfit",
+  '& a':{
+    transition: 'all 0.1s ease-in-out',
+  }
+});
+
+export const card = css({
+  bg: "neutral.nft-500",
+  height: "90%",
+  w: "auto",
+  padding: "1.5rem",
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: '300px',
+  borderRadius: "10px",
+  boxShadow: "rgba(0, 0, 0, 0.35) 0 25px 50px ",
+  color: "primary.nft-blue",
+});
+
+export const image = square({
+  width: "100%",
+  borderRadius: "10px",
+});
+
+export const separator = css({
+  width: "100%",
+  bg: "white",
+  opacity: "0.1",
+  height: "1px",
+  marginY: "18px",
+});
+
+export const cardBody = flex({
+  flexDirection: "column",
+  paddingTop: "1.5rem",
+  gap: "10px",
+  lineHeight: "loose",
+  "& div": {
+    display: "flex",
+    w: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    "& span": {
+      display: "flex",
+      gap: "4px",
+      alignItems: "center",
+    },
+    "& span.ethereum": {
+      color: "primary.nft-cyan",
+    },
+  },
+});
+
+export const layer = css({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  w: "full",
+  h: "full",
+  bg: "primary.nft-cyan/30",
+  borderRadius: "8px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
+  opacity: 0,
+  transition: 'opacity 0.1s ease-in-out',
+  "&:hover": {
+    opacity: 1, // Cambiar opacidad solo en hover
+  },
+});
+
+export const cardFooter = css({
+  display: "flex",
+  w: "100%",
+  gap: "10px",
+  alignItems: "center",
+  "& img": {
+    border: "solid 1px ",
+    borderRadius: "full",
+    borderColor: "primary.cyan",
+  },
+});
